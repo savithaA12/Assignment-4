52601c411e95ce794e4c8776a267a5e13996afaa
savitha12 <savithaa24082000@gmail.com>
2022-03-25 12:27:23 +0530

    Assignment 4

---
+++ Day4-corejava_assignment-main/Harman_Question1/FrequencyMain.java
@@ -0,0 +1,12 @@
+package Harman_Question1;
+
+public class FrequencytMain
+{
+   static PrintFrequency f;
+
+	public static void main(String[] args) {
+	
+        System.out.println("The frequency of each word in the file");		
+		f=new PrintFrequency();
+}
+}
